25423eb162b9f6cca094447c94b6310149ac2656
FebFlyer <51185691+febflyer@users.noreply.github.com>
2020-06-02 19:07:39 +0800

    Update AlipaySmileModel.java
    Key需要向阿里申请

--- app/src/main/java/com/sunmi/sunmik1demo/model/AlipaySmileModel.java
+++ app/src/main/java/com/sunmi/sunmik1demo/model/AlipaySmileModel.java
@@ -29,10 +29,10 @@ import openapi.response.AlipayTradePayResponse;
  */
 
 public class AlipaySmileModel {
-    public static String merchantId = "TEST_ZOLOZ_" + "2088521507558680";
-    public static String appKey = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCGwedKn8ClEJWNkuOsWpbyAvjC2zBVOTFy0V0He/PGh6+i8Dpr3p79T+qa0yMPn/YTnunX6zQ8+U70KVYhtb8AM/MOm0hIKxpVk3j7fifzhnjykACbObd17raYVFPoE0V9lo1e+68nYxXGDM2dpnBADL3/YmijOPy3bOJfl9vGY+og0N69t+TmvjpUAyW01UICCpCqYec0cnGqR4+mX7E07XoOn1Q/o6UkqYsN9YZDjd0o6gYr0OmHBl7mj98sDYf7lj6eIVIltlqzSdHETGWQZ3gyHtnySodtk5XMEYK4dtaiV4IGGjt7uO/U8PYmKeGjHcKt5rxxjEqUDAMurwOdAgMBAAECggEAKTmOmVyv4Y9mQQ5LykFYZicUlP/bFGdPwlFcRVrdI8IEsDMPnEpZlyLooX6v/iqwbCY0YZKZw4F9buJqcBual+RMmg5VGYbeIonf/AgHDs/3ljUJinMotdJWV9mMZbXnj1kMXJ9gYQtF8PWfvXfaZS3Avi0L18sqPK75vcbGPDWIfqK/75Nrn5t46R32ebB7cG7eXZpqhhH1gpEsVnDvYq3vBX7e81az0tWudXzqa3kXaMJF0N+iw5kRpwz77BZPn6oHPyl2ShUTNMsDjJeGbT5EtpSdD/GGrhFZvcIPpFJyXgRSRRev9Vb6Rw/92p86HHtKyslmZC5/FuaLeh9rvQKBgQDN5H5iRI1od1KR5XHZE2LujhpBlf8QO8sBfCZ9uVUSKBspBgsTBIGSVYChv/qqOW7mRawMPNLlMKZtn1RdDDiOybBx9EWLH1lhQ8lR3A2/wfypat1MK2QAleKj2NRHuQb3zEE4Wesvmw2+pM2bcXVRm5z1fhozPDblZgF9vKCeTwKBgQCnjY29EOroeJMRf4arD0vgs5Y6afB0uPuqgD+0EWqEPZ6YIxNZUqhSWi3cosjyopx4OVHwcc1PFR2x+CxW43iiPYJEua7DD0IhfiHQsy1OugRGhnnoHwxW9wz/SsVxKGXdHo2yeRI2x7guzL4znO3O5LdLfm0gN23YB4XF2u1QUwKBgQChaQozm66IQVM4EXppRat/fWPUGvV1EosZxOygD5VDwLFaPeScqiGySNmo9MpcfN4WJHS2u1BSYQt4UTpgH96PAksTav7CDHeTqwK+7BbTSA79+ks3FaU2WErvTGNPPxzZUwWmWcfo8FOcK7MMs6vXqHkqdAuyMviOaqOsoR4CPQKBgE77Rh4ilGkjNUhhUeze2AjC/wToHWYYMOHNdLoAH7LYjSLwHdXURe4T/lxsC8d8ab+uXXa3Xm1x6b6T2urWbW4lCnw0/zGJxsWc/HmxjKD4xL0t9Nc7k4iHCoRpDatqvKHAHlFMCw7xlFvTPbF8vwjw/YpDvVMbbdcQrLl4mrdrAoGBAL5jnfxxA1+JY8C7DZq5LZcTJntdD3Dpxgkec6+oI08F/OqrAnO0FETfuXsfzPsljjcFVaKSUUuH5ewwdArU8ExEc48xD7e5FaK0h14Z61z8e9kHZG5y3t+mUXP0LLp53r6MLnE+sfFD12ZVfXKqpBcDjCcZZGi1PeVkzYGFJFKK";
-    public static String Public_Key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhsHnSp/ApRCVjZLjrFqW8gL4wtswVTkxctFdB3vzxoevovA6a96e/U/qmtMjD5/2E57p1+s0PPlO9ClWIbW/ADPzDptISCsaVZN4+34n84Z48pAAmzm3de62mFRT6BNFfZaNXvuvJ2MVxgzNnaZwQAy9/2Joozj8t2ziX5fbxmPqINDevbfk5r46VAMltNVCAgqQqmHnNHJxqkePpl+xNO16Dp9UP6OlJKmLDfWGQ43dKOoGK9DphwZe5o/fLA2H+5Y+niFSJbZas0nRxExlkGd4Mh7Z8kqHbZOVzBGCuHbWoleCBho7e7jv1PD2Jinhox3Crea8cYxKlAwDLq8DnQIDAQAB";
-    public static String appId =  "2018033002474060";
+    public static String merchantId = "TEST_ZOLOZ_" + "2088";
+    public static String appKey = "MIKK";
+    public static String Public_Key = "MIAB";
+    public static String appId =  "2060";
     public static String payMethod = "zoloz.authentication.customer.smilepay.initialize";
     public static String version = "1.0";
     public static final String url = "https://openapi.alipay.com/gateway.do";
